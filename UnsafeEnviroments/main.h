a19875da5241bec71d17e03dd65dacaacb5e355a
zinx <63958372+zinx28@users.noreply.github.com>
2025-01-16 20:34:45 +0000

    pretty sure this works without minhook

--- UnsafeEnviroments/main.h
+++ UnsafeEnviroments/main.h
@@ -16,7 +16,7 @@ DWORD WINAPI Main(LPVOID)
 	FILE* f;
 	freopen_s(&f, "CONOUT$", "w", stdout);
 
-	MH_Initialize();
+	Memcury::VEHHook::Init();
 
 	//UnsafeEnvironment_Body
 	auto AttemptFind = Memcury::Scanner::FindStringRef(L"UnsafeEnvironment_Title").ScanFor({ 0x4C, 0x8B, 0xDC, 0x55, 0x49, 0x8D, 0xAB }, false).Get();
@@ -25,12 +25,15 @@ DWORD WINAPI Main(LPVOID)
 
 	//FPlatformMisc::RequestExitWithStatus(%i, %d) - return code will be ignored by the generic implementation.
 	//FPlatformMisc::RequestExit(%i)
-	auto AttemptFind2 = Memcury::Scanner::FindStringRef(L"FPlatformMisc::RequestExit(%i)").ScanFor({ 0x40, 0x53, 0x48, 0x83, 0xEC, 0x30, 0x80, 0x3D }, false).Get();
+	// Cannot find a compatible Vulkan device or driver. Try updating your video driver to a more recent version and make sure your video card supports Vulkan.\n\n
+	// 0xE8, 0x00, 0x00, 0x00, 0x00, 0xB2
+	// chapter 3 isnt finding it !! gr
+	auto AttemptFind2 = Memcury::Scanner::FindStringRef(L"Cannot find a compatible Vulkan device or driver. Try updating your video driver to a more recent version and make sure your video card supports Vulkan.\n\n").ScanFor({ 0xE8 }, true, 1).RelativeOffset(1).Get();
 	std::cout << "RequestExit: " << std::hex << AttemptFind2 << std::endl;
 	std::cout << "RequestExitNEW: " << std::hex << AttemptFind2 - __int64(GetModuleHandleW(0)) << std::endl;
 	
-	MH_CreateHook(LPVOID(AttemptFind), UnsafeEnvironment, nullptr);
-	MH_CreateHook(LPVOID(AttemptFind2), sub_7FF6BAD8D030, nullptr);
+	Memcury::VEHHook::AddHook(LPVOID(AttemptFind), UnsafeEnvironment);
+	Memcury::VEHHook::AddHook(LPVOID(AttemptFind2), sub_7FF6BAD8D030);
 	
 	if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK) {
 		std::cout << "Failed to enable hooks" << std::endl;
